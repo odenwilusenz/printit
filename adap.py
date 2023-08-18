09860f92292aed7b0d13aefc79a3b965a3424c42
user <yair99@gmail.com>
2023-08-18 19:18:43 +0300

    invert

--- adap.py
+++ adap.py
@@ -4,26 +4,12 @@ import cv2
 from PIL import Image
 import subprocess, os, base64
 from st_clickable_images import clickable_images
+import io
 
-def threshold_and_smooth_image(image, threshold_value, smoothing_kernel_size, invert_image):
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Check if the invert_image state exists, if not, create it
+if 'invert_image' not in st.session_state:
+    st.session_state.invert_image = False
 
-    # Apply binary thresholding with the selected threshold value
-    _, thresholded_image = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
-
-    # Apply smoothing using Gaussian blur with the selected kernel size
-    smoothed_image = cv2.GaussianBlur(thresholded_image, (smoothing_kernel_size, smoothing_kernel_size), 0)
-
-    # Invert the image if the option is selected
-    if invert_image:
-        smoothed_image = 255 - smoothed_image
-
-    return smoothed_image
-
-def print_image(file_path):
-    command = f"brother_ql -b pyusb --model QL-570 -p usb://0x04f9:0x2028/000M6Z401370 print -l 62 \"{file_path}\""
-    subprocess.run(command, shell=True)
 
 @st.cache_data
 def load_thumbnails():
@@ -35,59 +21,106 @@ def load_thumbnails():
         thumbnails.append(thumbnail)
     return thumbnails
 
+def threshold_and_smooth_image(image, threshold_value, smoothing_kernel_size, invert_image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresholded_image = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
+    smoothed_image = cv2.GaussianBlur(thresholded_image, (smoothing_kernel_size, smoothing_kernel_size), 0)
+    if invert_image:
+        smoothed_image = 255 - smoothed_image
+    return smoothed_image
+
+def print_image(file_path):
+    command = f"brother_ql -b pyusb --model QL-570 -p usb://0x04f9:0x2028/000M6Z401370 print -l 62 \"{file_path}\""
+    subprocess.run(command, shell=True)
+
+def image_to_data_url(image_input):
+    if isinstance(image_input, np.ndarray): # NumPy array
+        if len(image_input.shape) == 2 or image_input.shape[2] == 1: # Grayscale
+            image_pil = Image.fromarray(image_input.astype('uint8'), mode='L')
+        else: # Color
+            image_pil = Image.fromarray(image_input.astype('uint8'))
+        buffer = io.BytesIO()
+        image_format = 'png'
+        image_pil.save(buffer, format=image_format)
+        image_data = buffer.getvalue()
+    else: # File path
+        with open(image_input, "rb") as image_file:
+            image_data = image_file.read()
+            image_format = os.path.splitext(image_input)[1].lstrip(".").lower()
+    
+    base64_image = base64.b64encode(image_data).decode()
+    data_url = f"data:image/{image_format};base64,{base64_image}"
+    return data_url
+
+
 # Title of the app
 st.title('image editor for stickerPrinter')
 
 # Upload the image
 uploaded_image = st.file_uploader("Choose an image file (png/jpg/gif)", type=['png', 'jpg', 'gif'])
 
+
 if uploaded_image is not None:
-    # Read the uploaded image
     image = Image.open(uploaded_image).convert('RGB')
     image_np = np.array(image)
-
-    # Sliders
     threshold_value = st.slider('Threshold Value', min_value=0, max_value=255, value=127)
-    # smoothing_kernel_size = st.slider('Smoothing Kernel Size', min_value=1, max_value=9, value=3, step=2)
     smoothing_kernel_size = 3
-    # Invert button
-    invert_image = st.button('Invert Image')
+
+    if 'invert_image' not in st.session_state:
+        st.session_state.invert_image = False
+        
+    if st.button('Invert Image'):
+        st.session_state.invert_image = not st.session_state.invert_image
+
 
     # Threshold and smooth the image with the selected values
-    processed_image = threshold_and_smooth_image(image_np, threshold_value, smoothing_kernel_size, invert_image)
+    processed_image = threshold_and_smooth_image(image_np, threshold_value, smoothing_kernel_size, st.session_state.invert_image)
 
-    # Display the images side by side
+    original_image_data_url = image_to_data_url(image_np)
+    processed_image_data_url = image_to_data_url(processed_image)
+
     col1, col2 = st.columns(2)
-    col1.image(image_np, caption='Original Image', use_column_width=True)
-    col2.image(processed_image, caption=f'Processed Image (Threshold: {threshold_value}, Smoothing Kernel: {smoothing_kernel_size})', use_column_width=True, channels='GRAY')
 
-    # Print button
-    if st.button('Print Processed Image'):
-        # Save the processed image
-        file_path = 'temp/processed_image.png'
-        cv2.imwrite(file_path, processed_image)
+    original_image_data_url = image_to_data_url(image_np)
+    clicked_original = clickable_images(
+        [original_image_data_url],
+        titles=["Original Image"],
+        div_style={"display": "flex", "justify-content": "center"},
+        img_style={"margin": "5px", "height": "200px"},
+    )
 
-        # Create and save the thumbnail
-        thumbnail_path = 'temp/thumbnails/processed_image_thumbnail.png'
-        image_pil = Image.fromarray(processed_image)
-        thumbnail_size = (100, 100)
-        image_pil.thumbnail(thumbnail_size)
-        image_pil.save(thumbnail_path)
+    processed_image_data_url = image_to_data_url(processed_image) # Using processed_image directly
+    clicked_processed = clickable_images(
+        [processed_image_data_url],
+        titles=[f'Processed Image (Threshold: {threshold_value}, Smoothing Kernel: {smoothing_kernel_size})'],
+        div_style={"display": "flex", "justify-content": "center"},
+        img_style={"margin": "5px", "height": "200px"},
+    )
 
-        # Print the image using the provided function
-        print_image(file_path)
+    
+    if clicked_original > -1:
+        original_file_path = uploaded_image.name
+        print_image(original_file_path)
+        st.success(f'Original image sent to printer!')
 
-        st.success('Image sent to printer!')
-def image_to_data_url(image_path):
-    with open(image_path, "rb") as image_file:
-        base64_image = base64.b64encode(image_file.read()).decode()
-        image_format = os.path.splitext(image_path)[1].lstrip(".").lower()
-        data_url = f"data:image/{image_format};base64,{base64_image}"
-        return data_url
+    if clicked_processed > -1:
+        adapted_file_path = os.path.join('temp/', os.path.basename(uploaded_image.name).split('.')[0] + '_adapted.png')
+        cv2.imwrite(adapted_file_path, processed_image)
+        print_image(adapted_file_path)
+        st.success(f'Processed image sent to printer and saved as {adapted_file_path}!')
 
-# Get the list of image files in the "temp/" folder
-image_files = [f for f in os.listdir('temp/') if f.endswith(('.png', '.jpg', '.gif'))]
-image_data_urls = [image_to_data_url(os.path.join('temp/', image_file)) for image_file in image_files]
+
+# Get the full paths of image files in the "temp/" folder
+image_files_full_paths = [os.path.join('temp/', f) for f in os.listdir('temp/') if f.endswith(('.png', '.jpg', '.gif'))]
+
+# Sort the image files by last modification time, then reverse the order
+image_files_full_paths.sort(key=os.path.getmtime, reverse=True)
+
+# Extract the filenames from the sorted full paths
+image_files = [os.path.basename(image_file) for image_file in image_files_full_paths]
+
+# Generate the data URLs for the sorted image files
+image_data_urls = [image_to_data_url(image_file) for image_file in image_files_full_paths]
 image_titles = [f"{image_file}" for image_file in image_files]
 
 # Display the clickable thumbnails
@@ -100,10 +133,9 @@ clicked = clickable_images(
 
 # Handle the click event
 if clicked > -1:
-    clicked_image_path = os.path.join('temp/', image_files[clicked])
+    clicked_image_path = image_files_full_paths[clicked]
     print_image(clicked_image_path)
     st.success(f'Image {image_files[clicked]} sent to printer!')
-
 
 
 
